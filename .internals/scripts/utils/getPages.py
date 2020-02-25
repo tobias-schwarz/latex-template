4b07bfd415982d7cba167c6c64540b0b39947616
tobias-schwarz <tobias.schwarz@gmx.org>
2020-02-25 13:19:12 +0100

    Test outputs

---
+++ .internals/scripts/utils/getPages.py
@@ -0,0 +1,10 @@
+import PyPDF2
+NOT_COUNTING_PAGES = 16
+pdf_file = open('master.pdf', 'rb')
+read_pdf = PyPDF2.PdfFileReader(pdf_file)
+number_of_pages = read_pdf.getNumPages() - NOT_COUNTING_PAGES
+page_file = open('.internals/scripts/utils/pages.txt', 'w+')
+progress_file = open('.internals/scripts/utils/progress.txt', 'w+')
+page_file.write(str(number_of_pages) + "\n")
+progress_file.write(str(int(number_of_pages / 80 * 100)) +
+                    '%-' + str(int(number_of_pages / 60 * 100)) + '%\n')
